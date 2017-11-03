9ccb51febfdb5d87ab1bbbe01588d99eb03bde1d
Maosen <maosen1993@gmail.com>
2017-11-03 00:56:17 -0400

    refactor

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,28 +1,28 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-  var quote = '';
-  var author = '';
-  var colors = ['#8B0000', '#006400', '#00008B', '#000', '#B8860B'];
+$(() => {
+  let quote = '';
+  let author = '';
+  const colors = ['#8B0000', '#006400', '#00008B', '#000', '#B8860B'];
   function getQuotes() {
-    var url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
-    $.getJSON(url, function(json) {
+    const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+    $.getJSON(url, json => {
       quote = json.quoteText;
       author = json.quoteAuthor;
-      $('.quote').html('"' + quote + '"');
-      $('.author').html('- ' + author);
-      var index = Math.floor(Math.random() * 5);
-      var color = colors[index];
+      $('.quote').html(`"${quote}"`);
+      $('.author').html(`- ${author}`);
+      const index = Math.floor(Math.random() * 5);
+      const color = colors[index];
       $('body').css('background-color', color);
       $('.quotes').css('color', color);
       $('.btn').css({ 'background-color': color, color: 'white' });
     });
   }
   getQuotes();
-  $('#twitter_btn').on('click', function() {
-    window.open('https://twitter.com/intent/tweet?text=' + '"' + quote + '"' + ' - ' + author + '&hashtags=quote');
+  $('#twitter_btn').on('click', () => {
+    window.open(`https://twitter.com/intent/tweet?text="${quote}" - ${author}&hashtags=quote`);
   });
-  $('#new_quote').on('click', function() {
+  $('#new_quote').on('click', () => {
     getQuotes();
   });
 });
